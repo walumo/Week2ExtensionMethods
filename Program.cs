ca138f7e2087f1c12e5a75f2e8b6bbef723c8a80
walumo <miikka.hakulinen@awacademy.training>
2022-06-16 14:33:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+
+namespace Game
+{
+    public class Raffle
+    {
+        public bool victory;
+
+        // display welcome message
+        public void Welcome()
+        {
+            Console.WriteLine("\n\n*********************************");
+            Console.WriteLine("Welcome to the lotto game\n");
+        }
+
+        // show dots while raffling
+        static void ShowDots()
+        {
+            int i = 0;
+            while (i < 3)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000);
+                i++;
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        void GetResult()
+        {
+            var rand = new Random();
+            if (rand.Next(100) > 70)
+            {
+                this.victory = true;
+            }
+            else
+            {
+                this.victory = false;
+            }
+        }
+        void GetResult(int guess = 0)
+        {
+            var rand = new Random().Next(10);
+
+            if (rand == guess)
+            {
+                this.victory = true;
+                Console.WriteLine("Raffle result was: {0}", rand);
+            }
+            else
+            {
+                this.victory = false;
+                Console.WriteLine("Raffle result was: {0}", rand);
+            }
+        }
+
+        // play the game
+        public void Play()
+        {
+            Console.Write("Enter number between 1 and 10: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            //Console.ReadKey(true);
+            Console.Write("Raffling ");
+            ShowDots();
+            GetResult(userInput);
+        }
+
+
+        
+    }
+    public static class MyExtension
+    {
+        public static void GivePrize(this bool hasWon)
+        {
+            if (hasWon) Console.WriteLine("You won 3 million alpacas!");
+            else Console.WriteLine("You != winner...");
+        }
+    }
+
+
+    class Exercise
+    {
+        static void Main(string[] args)
+        {
+
+            Raffle lotto = new Raffle();
+
+            lotto.Welcome();
+            lotto.Play();
+            
+            lotto.victory.GivePrize();           
+          
+
+            Console.WriteLine("*********************************");
+        }
+    }
+
+
+}
